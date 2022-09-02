e40792ea95a98da2268d486017735c3c661e40a6
liuliwei <liulw015@avic.com>
2022-09-02 13:43:02 +0800

    修改模板

--- config/index.js
+++ config/index.js
@@ -70,7 +70,7 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    { "keyword": "love_day", date: "2015-05-01" },
+    { "keyword": "love_day", date: "2021-09-04" },
     // 结婚纪念日
     // {"keyword": "marry_day", date: "2020-01-04"},
     // 退伍日, 不用可以删掉
